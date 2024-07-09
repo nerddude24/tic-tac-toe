1e0d516e0f1ecbee2f2dd2840948cb894ac872f5
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-07-09 14:35:29 +0100

    Add Game object with some of it's functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const Gameboard = (function () {
-	board = ["", "", "", "", "", "", "", "", ""];
+	let board;
+	const X = "X";
+	const O = "O";
 
 	const getBoard = () => board;
 
@@ -14,7 +16,7 @@ const Gameboard = (function () {
 			return;
 		}
 
-		if (!["X", "O"].includes(value)) {
+		if (![X, O].includes(value)) {
 			console.warn("Invalid value!");
 			return;
 		}
@@ -22,8 +24,61 @@ const Gameboard = (function () {
 		board[cellNum] = value;
 	};
 
+	const clearBoard = () => {
+		if (Game.getPlaying) {
+			console.warn("Tried to clear board during a game!");
+			return;
+		}
+		board = ["", "", "", "", "", "", "", "", ""];
+	};
+
+	clearBoard();
+
 	return {
 		getBoard,
 		fillCell,
+		clearBoard,
 	};
+})();
+
+const Game = (function () {
+	let playing = false;
+	const playerOne = createPlayer("X");
+	const playerTwo = createPlayer("O");
+	let activePlayer = playerOne;
+
+	if (playerOne === null || playerTwo === null) {
+		console.error("One or both of the players are null, stopping game...");
+		return;
+	}
+
+	const getPlaying = () => playing;
+
+	const createPlayer = function (value) {
+		if (![Gameboard.X, Gameboard.O].includes(value)) {
+			console.error(
+				`Tried to create a player with an invalid value ('${value}')!`
+			);
+			return null;
+		}
+		return { value };
+	};
+
+	const swapActivePlayer = () => {
+		activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+	};
+
+	const startRound = () => {
+		let movesLeft = 9;
+		Gameboard.clearBoard();
+		playing = true;
+
+    for (let moveIdx = 1; moveIdx <= movesLeft; moveIdx++) {
+      
+    }
+
+    playing = false;
+	};
+
+	return { getPlaying };
 })();
