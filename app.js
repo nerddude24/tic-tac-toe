54a4b748b9a845ce0a5a0f868552e381e9e8ccf9
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-07-09 15:13:30 +0100

    Handle player input and fill cells

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const Gameboard = (function () {
-	let board;
+	let board = ["", "", "", "", "", "", "", "", ""];
 	const X = "X";
 	const O = "O";
 
 	const getBoard = () => board;
+
+	const isValidCellValue = (val) => [Gameboard.X, Gameboard.O].includes(val);
 
 	const fillCell = (cellNum, value) => {
 		if (cellNum < 0 || cellNum > 8) {
@@ -16,7 +18,7 @@ const Gameboard = (function () {
 			return;
 		}
 
-		if (![X, O].includes(value)) {
+		if (!isValidCellValue(value)) {
 			console.warn("Invalid value!");
 			return;
 		}
@@ -32,19 +34,30 @@ const Gameboard = (function () {
 		board = ["", "", "", "", "", "", "", "", ""];
 	};
 
-	clearBoard();
-
 	return {
 		getBoard,
 		fillCell,
 		clearBoard,
+		isValidCellValue,
+		X,
+		O,
 	};
 })();
 
 const Game = (function () {
+	const createPlayer = function (name, value) {
+		if (!Gameboard.isValidCellValue(value)) {
+			console.error(
+				`Tried to create a player with an invalid value ('${value}')!`
+			);
+			return null;
+		}
+		return { name, value };
+	};
+
 	let playing = false;
-	const playerOne = createPlayer("X");
-	const playerTwo = createPlayer("O");
+	const playerOne = createPlayer("Player One", Gameboard.X);
+	const playerTwo = createPlayer("Player Two", Gameboard.O);
 	let activePlayer = playerOne;
 
 	if (playerOne === null || playerTwo === null) {
@@ -53,16 +66,6 @@ const Game = (function () {
 	}
 
 	const getPlaying = () => playing;
-
-	const createPlayer = function (value) {
-		if (![Gameboard.X, Gameboard.O].includes(value)) {
-			console.error(
-				`Tried to create a player with an invalid value ('${value}')!`
-			);
-			return null;
-		}
-		return { value };
-	};
 
 	const swapActivePlayer = () => {
 		activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
@@ -76,30 +79,50 @@ const Game = (function () {
 		console.log("Game started!");
 
 		for (let moveIdx = 1; moveIdx <= movesLeft; moveIdx++) {
-			ConsoleDisplay.printBoard();
+			ConsoleManager.printBoard();
+
+			let cellNum;
+			while (true) {
+				cellNum = prompt(
+					`${activePlayer.name}'s move, where do you want to play? (1-9):`
+				);
+
+				if (cellNum < 1 || cellNum > 9) {
+					alert("Invalid move! try again.");
+				} else break;
+			}
+
+			// -1 because arrays start at 0, and because 1-9 is easier to thing about
+			Gameboard.fillCell(cellNum - 1, activePlayer.value);
+			swapActivePlayer();
 		}
 
 		playing = false;
+		console.log("Game is done!");
 	};
 
 	return { getPlaying, playRound };
 })();
 
-const ConsoleDisplay = (function () {
+const ConsoleManager = (function () {
 	const printBoard = () => {
 		const board = Gameboard.getBoard();
-		let printedString = "";
+		console.log(board);
+		/* let printedString = "";
 
 		for (let i = 0; i < 3; i++) {
 			for (let j = 0; j < 3; j++) {
-				printedString += board[i * j] + " | ";
+				printedString += board[i + j] + " | ";
 			}
 			printedString += "\n";
 			printedString += "----------";
+			printedString += "\n";
 		}
 
-		console.log(printedString);
+		console.log(printedString); */
 	};
 
 	return { printBoard };
 })();
+
+Game.playRound();
