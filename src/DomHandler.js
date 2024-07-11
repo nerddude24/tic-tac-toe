3008376a2f5aa6fddd2edbe1a593ad685e3fff49
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-07-11 12:10:12 +0100

    Add updating stats functionality to DomHandler

--- src/DomHandler.js
+++ src/DomHandler.js
@@ -1,9 +1,14 @@
-const DomHandler = (function () {
+(function () {
 	const boardDom = document.querySelector("#board");
-
 	const boardCells = Array.from(boardDom.children);
+	const scoreOneElement = document.querySelector("#stats1 > player-score");
+	const scoreTwoElement = document.querySelector("#stats2 > player-score");
+	const nameOneElement = document.querySelector("#stats1 > player-name");
+	const nameTwoElement = document.querySelector("#stats2 > player-name");
 
-	eventHandler.subscribe("boardChanged", _render);
+	eventHandler.subscribe("boardChanged", updateBoard);
+	eventHandler.subscribe("scoreChanged", updateScore);
+	eventHandler.subscribe("nameChanged", updateName);
 
 	boardCells.forEach((cell, idx) => {
 		cell.addEventListener("click", () => {
@@ -11,11 +16,19 @@ const DomHandler = (function () {
 		});
 	});
 
-	function _render(board) {
+	function updateBoard(board) {
 		for (let i = 0; i <= 8; i++) {
 			boardCells[i].textContent = board[i];
 		}
 	}
 
-	return {};
+	function updateScore(scoreOne, scoreTwo) {
+		scoreOneElement.textContent = scoreOne;
+		scoreTwoElement.textContent = scoreTwo;
+	}
+
+	function updateName(nameOne, nameTwo) {
+		nameOneElement.textContent = nameOne;
+		nameTwoElement.textContent = nameTwo;
+	}
 })();
